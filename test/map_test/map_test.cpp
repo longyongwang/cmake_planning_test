07a5ab8a40aa2cbed3701e7732952250418309ac
wangyonglong <wangyonglongfeng@163.com>
2025-05-06 02:22:31 +0800

    add test

---
+++ test/map_test/map_test.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "pnc_map.h"
+using std::cout;
+using std::endl;
+
+void mapTest()
+{
+    cout << "this is pnc_map test" << endl;
+    PNC_Map mymap;
+    mymap.mapInfo();
+}
+ int main()
+ {
+    mapTest();
+    return 0;
+ }
