3a47f4f48a73c83ad52ec79e3ae5994e78d18c6b
wangyonglong <wangyonglongfeng@163.com>
2025-05-06 01:44:34 +0800

    call easyx lib

--- src/planning_main.cpp
+++ src/planning_main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include"process.h"
+#include "show_result.h"
 using std::cout;
 using std::endl;
 
@@ -9,6 +10,8 @@ int main()
     Process pro;
     pro.planProcess();
     cout << "planning end" << endl;
-
+    cout << "show result:" << endl;
+    ShowResult show;
+    show.drawResult();
     return 0;
 }
